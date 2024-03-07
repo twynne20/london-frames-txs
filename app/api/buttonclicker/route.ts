0a12588aff9bc5da4308d0a7a373043cf73a6b1f
Brian Doyle <brian.doyle@coinbase.com>
2024-03-07 16:43:53 -0500

    Remove logs and parse as gwei

--- app/api/buttonclicker/route.ts
+++ app/api/buttonclicker/route.ts
@@ -1,6 +1,6 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
-import { encodeFunctionData, formatEther, parseEther } from 'viem';
+import { encodeFunctionData, formatEther, parseGwei } from 'viem';
 import { base } from 'viem/chains';
 import type { FrameTransactionResponse } from '@coinbase/onchainkit/frame';
 
@@ -8,14 +8,12 @@ import ClickTheButtonABI from '../../_contracts/ClickTheButtonAbi';
 import { CLICK_THE_BUTTON_CONTRACT_ADDR } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
-  console.log('Getting response');
   const body: FrameRequest = await req.json();
   const { isValid } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
   
   if (!isValid) {
     return new NextResponse('Message not valid', { status: 500 });
   }
-  console.log('Message is valid');
 
   const data = encodeFunctionData({
     abi: ClickTheButtonABI,
@@ -29,10 +27,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
       abi: [],
       data,
       to: CLICK_THE_BUTTON_CONTRACT_ADDR,
-      value: formatEther(parseEther('0.00001')), // 0.00001 ETH
+      value: formatEther(parseGwei('10000')), // 0.00001 ETH
     },
   };
-  console.log('Returning txData');
   return NextResponse.json(txData);
 }
 
