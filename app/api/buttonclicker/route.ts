a07a744cc221ad056a5348744ae4015e82f53899
Brian Doyle <brian.doyle@coinbase.com>
2024-03-07 11:44:23 -0500

    Add buttonclicker

---
+++ app/api/buttonclicker/route.ts
@@ -0,0 +1,40 @@
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+import { encodeFunctionData, formatEther, parseEther } from 'viem';
+import { base } from 'viem/chains';
+import type { FrameTransactionResponse } from '@coinbase/onchainkit/frame';
+
+import ClickTheButtonABI from '../../_contracts/ClickTheButtonAbi';
+import { CLICK_THE_BUTTON_CONTRACT_ADDR } from '../../config';
+
+async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
+  const body: FrameRequest = await req.json();
+  const { isValid } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+  
+  if (!isValid) {
+    return new NextResponse('Message not valid', { status: 500 });
+  }
+
+  const data = encodeFunctionData({
+    abi: ClickTheButtonABI,
+    functionName: 'clickTheButton',
+  });
+
+  const txData: FrameTransactionResponse = {
+    chainId: `eip155:${base.id}`,
+    method: 'eth_sendTransaction',
+    params: {
+      abi: [],
+      data,
+      to: CLICK_THE_BUTTON_CONTRACT_ADDR,
+      value: formatEther(parseEther('0.00001')), // 0.00001 ETH
+    },
+  };
+  return NextResponse.json(txData);
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
---
+++ app/buttonclicker/page.tsx
@@ -0,0 +1,48 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from '../config';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      action: 'tx',
+      label: 'Click the Button',
+      target: `${NEXT_PUBLIC_URL}/api/buttonclicker`,
+    },
+    {
+      action: 'link',
+      label: 'Leaderboard',
+      target: `${NEXT_PUBLIC_URL}/buttonclicker`,
+    },
+  ],
+  image: {
+    src: `${NEXT_PUBLIC_URL}/button.webp`,
+    aspectRatio: '1:1',
+  },
+  input: {
+    text: 'Don\'t click the button!',
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+});
+
+export const metadata: Metadata = {
+  title: 'Click the Button',
+  description: "Don't click the button!",
+  openGraph: {
+    title: 'Click the Button',
+    description: "Don't click the button!",
+    images: [`${NEXT_PUBLIC_URL}/button.webp`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Leaderboard</h1>
+      <p>TODO</p>
+    </>
+  );
+}
