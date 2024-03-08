77be83b5ad8b77d330b1bce88b635220049c4128
Brian Doyle <brian.doyle@coinbase.com>
2024-03-08 13:30:06 -0500

    Implement Taylor's fix for list

--- app/buttonclicker/page.tsx
+++ app/buttonclicker/page.tsx
@@ -5,6 +5,7 @@ import { createPublicClient, encodeFunctionData, http } from 'viem';
 import { base } from 'viem/chains';
 import ClickTheButtonABI from '../_contracts/ClickTheButtonAbi';
 import { CLICK_THE_BUTTON_CONTRACT_ADDR } from '../config';
+import { useState } from 'react';
 
 type Player = {
   user: string;
@@ -48,25 +49,31 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
+  const [list, setList] = useState<React.JSX.Element[]>([]);
+
   const publicClient = createPublicClient({
     chain: base,
     transport: http(),
   });
 
-  const players = await publicClient.readContract({
-    address: CLICK_THE_BUTTON_CONTRACT_ADDR,
-    abi: ClickTheButtonABI,
-    functionName: 'getAllClicks',
-  }) as Player[];
+  async function fetchPlayers() {
+    const players = (await publicClient.readContract({
+      address: CLICK_THE_BUTTON_CONTRACT_ADDR,
+      abi: ClickTheButtonABI,
+      functionName: 'getAllClicks',
+    })) as Player[];
 
-  // Sort players by clicks
-  players.sort((a, b) => parseInt(b.clicks) - parseInt(a.clicks));
+    // Sort players by clicks
+    players.sort((a, b) => parseInt(b.clicks) - parseInt(a.clicks));
 
-  const list = players.map((player, index) => {
+    const updatedList: React.JSX.Element[] = players.map((player, index) => {
       return <div>{`${index + 1}. ${player.user} - ${player.clicks}`}</div>;
-    }
-  );
+    });
 
+    setList(updatedList);
+  }
+
+  
   return (
     <>
       <h1>Leader Board</h1>
